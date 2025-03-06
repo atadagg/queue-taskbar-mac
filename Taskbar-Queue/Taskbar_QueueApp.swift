2fab2e9766e03c6082fd6142ae2d5ff5a76e99cc
Ata Tan Dagidir <tan.dagidir@ozu.edu.tr>
2025-03-06 16:37:51 +0300

    Initial Commit

---
+++ Taskbar-Queue/Taskbar_QueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taskbar_QueueApp.swift
+//  Taskbar-Queue
+//
+//  Created by Ata Tan Dagidir on 6.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Taskbar_QueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
